13c8a47b2744219c01a800409a854217558f00ef
snakazawa <nakazawa.tms0314@gmail.com>
2015-05-25 21:50:01 +0900

    リファクタリング

--- routes/github.js
+++ routes/github.js
@@ -12,36 +12,9 @@ var stages = require('../lib/model/stages');
 var routes = {
     issues: {
         // issue 作成
-        opened: function (req, res) {
-            var issue;
-            var projectId;
-
-            async.series([
-                // project
-                function (next) {
-                    Project.findOne({
-                        'github.userName': req.body.repository.owner.login,
-                        'github.repoName': req.body.repository.name
-                    }, function (err, doc) {
-                        if (err) { next(err); }
-                        else if (!doc) { next(new Error('project not found')); }
-                        else {
-                            projectId = doc._id;
-                            next(null);
-                        }
-                    });
-                },
-                // issue
-                function (next) {
-                    GitHub.serializeIssue(req.body.issue, function (err, data) {
-                        issue = data;
-                        next(err);
-                    });
-                }
-            ], function (err) {
-                if (err && err.message === 'project not found') {
-                    res.status(400).json({message: err.message});
-                } else if (err) {
+        opened: function (projectId, req, res) {
+            GitHub.serializeIssue(req.body.issue, function (err, issue) {
+                if (err) {
                     console.error(err);
                     res.status(500).json({message: err.message});
                 } else {
@@ -53,16 +26,30 @@ var routes = {
     }
 };
 
-router.post('/', function (req, res) {
+router.post('/:projectId', function (req, res) {
     var type = req.get('x-Github-Event');
     var action = req.body && req.body.action;
 
-    if (!~Object.keys(routes).indexOf(type) ||
-        !~Object.keys(routes[type]).indexOf(action)) {
-        res.status(400).end();
-    } else {
-        routes[type][action](req, res);
-    }
+    // projetの特定
+    Project.findOne({id: req.params.projectId}, function (err, project) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({message: err.message});
+            return;
+        }
+        if (!project) {
+            res.status(400).json({message: 'project not found'});
+            return;
+        }
+
+        // ルーティング
+        if (!~Object.keys(routes).indexOf(type) ||
+            !~Object.keys(routes[type]).indexOf(action)) {
+            res.status(400).end();
+        } else {
+            routes[type][action](project.id, req, res);
+        }
+    });
 });
 
 module.exports = router;
