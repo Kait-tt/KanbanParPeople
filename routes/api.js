d3f9e5e53ceed5420612b93c077408bb98e84c24
snakazawa <nakazawa.tms0314@gmail.com>
2015-08-16 09:19:06 +0900

    リファクタリング

--- routes/api.js
+++ routes/api.js
@@ -6,52 +6,31 @@ var GitHub = require('../lib/model/github');
 // Get Projects
 router.get('/projects', function (req, res) {
     Project.findPopulated({userName: req.username}, {}, function (err, projects) {
-        if (err) {
-            serverError(res, err);
-            return;
-        }
-
-        res.status(200).json({
-            message: 'OK',
-            projects: projects
-        });
+        if (err) { return serverError(res, err); }
+        res.status(200).json({ message: 'OK', projects: projects });
     });
 });
 
 // Get a Project
 router.get('/projects/:projectId', function (req, res) {
     Project.findPopulated({id: req.params.projectId}, {one: true}, function (err, project) {
-        if (err) {
-            serverError(res, err);
-            return;
-        }
-
-        res.status(200).json({
-            message: 'OK',
-            project: project
-        });
+        if (err) { return serverError(res, err); }
+        res.status(200).json({ message: 'OK', project: project });
     });
 });
 
 // Remove a Project
 router.delete('/projects/:projectId', function (req, res) {
     Project.remove({id: req.params.projectId}, function (err, project) {
-        if (err) {
-            serverError(res, err);
-            return;
-        }
-
-        res.status(200).json({
-            message: 'OK'
-        });
+        if (err) { return serverError(res, err); }
+        res.status(200).json({ message: 'OK' });
     });
 });
 
 // Import Project
 router.post('/projects', function (req, res) {
     if (!req.body.userName || !req.body.repoName) {
-        res.status(400).json({message: 'Required userName and repoName.'});
-        return;
+        return res.status(400).json({message: 'Required userName and repoName.'});
     }
 
     (new GitHub(req.user.token)).importProject(
@@ -59,30 +38,17 @@ router.post('/projects', function (req, res) {
         req.body.repoName,
         req.user.username,
         function (err, project) {
-            if (err) {
-                serverError(res, err);
-                return;
-            }
+            if (err) { return serverError(res, err); }
 
             Project.findPopulated({id: project.id}, {one: true}, function (err, doc) {
-                if (err) {
-                    serverError(res, err);
-                    return;
-                }
-
-                res.status(200).json({
-                    message: 'OK',
-                    project: doc
-                });
+                if (err) { return serverError(res, err); }
+                res.status(200).json({message: 'OK', project: doc });
             });
         });
 });
 
 function serverError(res, err) {
-    res.status(500).json({
-        message: 'server error.',
-        error: err.message
-    });
+    res.status(500).json({message: 'server error.', error: err.message });
 }
 
 module.exports = router;
