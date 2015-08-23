64c29ddb75522a5feb02cc5d15338dc3a6589548
snakazawa <nakazawa.tms0314@gmail.com>
2015-08-23 16:20:14 +0900

    簡易的なセットアップファイルを追加

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "### git pull ###" && \
+git pull && \
+echo "### bower install ###" && \
+bower install --allow-root && \
+echo "### npm install ###" && \
+npm install &&\
+echo "### compass compile ###" && \
+compass compile
+
