31e5b15f2df1d6f0e6df45ae52cd24961a8cea1d
snakazawa <nakazawa.tms0314@gmail.com>
2015-10-15 05:07:23 +0900

    logModelでapiなのにtypeがsetされていないものがあったので、これを修正するバッチを作成

---
+++ batch/serializeAPILog.js
@@ -0,0 +1,35 @@
+var _ = require('underscore');
+var config = require('config');
+var async = require('async');
+var mongoose = require('mongoose');
+var Log = require('../lib/model/log');
+
+console.log('### generate log model\'s text parameter ###');
+
+mongoose.connect(config.get('mongo.url'));
+
+
+async.series([
+    function (next) {
+        Log.find({}, function (err, logs) {
+            if (err) { return next(err); }
+            async.each(logs, function (log, nextLog) {
+                if (log.values.method && log.values.path) {
+                    console.log('set type: ', log.values, log.values.path);
+                    log.type = 'api';
+                    log.save(nextLog);
+                } else {
+                    nextLog();
+                }
+            }, next);
+        });
+    }
+
+], function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('done');
+    }
+    mongoose.disconnect();
+});
