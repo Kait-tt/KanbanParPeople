bb735ed93bed609658e523b52bbd52e7fd4b6328
snakazawa <nakazawa.tms0314@gmail.com>
2015-12-17 02:37:37 +0900

    setupファイルの修正

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-echo "### git pull ###" && \
-git pull && \
-echo "### bower install ###" && \
-bower install --allow-root && \
-echo "### npm install ###" && \
-npm install &&\
-echo "### compass compile ###" && \
+echo "### git pull ###"
+git pull
+echo "### bower install ###"
+bower install --allow-root
+echo "### npm install ###"
+npm install
+echo "### compass compile ###"
 compass compile
+forever restart 0
 
